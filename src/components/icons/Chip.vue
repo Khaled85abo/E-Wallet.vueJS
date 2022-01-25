55829610fb27647b8922cc42d537ed087b526b4e
Khaled Abo <khaledabo@Khaleds-MacBook-Pro.local>
2022-01-25 10:21:00 +0100

    Icons components fixed

---
+++ src/components/icons/Chip.vue
@@ -0,0 +1,22 @@
+<template>
+  <svg
+    width="50"
+    height="40"
+    viewBox="0 0 50 40"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <rect width="50" height="40" rx="8" :fill="iconsFill" fill-opacity="0.3" />
+    <line y1="12.5" x2="18" y2="12.5" :stroke="chipLine" />
+    <line x1="33" y1="12.5" x2="50" y2="12.5" :stroke="chipLine" />
+    <line x1="32.5" y1="5.46392e-08" x2="32.5" y2="40" :stroke="chipLine" />
+    <line y1="26.5" x2="18" y2="26.5" :stroke="chipLine" />
+    <line x1="32" y1="26.5" x2="50" y2="26.5" :stroke="chipLine" />
+    <line x1="18.5" y1="5.46392e-08" x2="18.5" y2="40" :stroke="chipLine" />
+  </svg>
+</template>
+<script>
+export default {
+  props: ["iconsFill", "chipLine"],
+};
+</script>
---
+++ src/components/icons/index.js
@@ -0,0 +1,7 @@
+import Bitcoin from "./Bitcoin.vue";
+import Blockchain from "./Blockchain.vue";
+import Evil from "./Evil.vue";
+import Ninja from "./Ninja.vue";
+import Chip from "./Chip.vue";
+import Wifi from "./Wifi.vue";
+export { Bitcoin, Blockchain, Evil, Ninja, Chip, Wifi };
